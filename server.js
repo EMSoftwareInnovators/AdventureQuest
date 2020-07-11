3bd3cc669e7a92b04a9e946de85ca8d39258326a
hbull5 <harcourtbullv@gmail.com>
2020-07-10 22:14:01 -0400

    Register Validation + Register DB Write

--- server.js
+++ server.js
@@ -1,19 +1,6 @@
-const keys = require("./config/keys");
 const express = require("express");
 const app = express();
-const session = require("express-session");
-const mysql = require("mysql");
-const mysqlStore = require("express-mysql-session")(session);
-const flash = require("connect-flash");
-
-// configure DB connection
-const connection = mysql.createConnection(keys.connectionObj);
-
-// establish DB connection
-connection.connect((err) => {
-	if (err) throw err;
-	console.log("MySQL DB connected...");
-});
+const PORT = process.env.PORT || 5000;
 
 // set ejs as view engine
 app.set("view engine", "ejs");
@@ -26,4 +13,4 @@ app.use("/", require("./routes/index"));
 app.use("/users", require("./routes/users"));
 
 // set server to listen
-app.listen(keys.PORT, () => console.log(`Server running on port ${keys.PORT}...`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}...`));
