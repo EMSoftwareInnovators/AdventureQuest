fa26259e3ae594463c4de2f2b12221f439086811
hbull5 <harcourtbullv@gmail.com>
2020-07-11 22:11:18 -0400

    added flash messaging + understand how it actually works

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
+const flash = require("connect-flash");
+const session = require("express-session");
 
 // set ejs as view engine
 app.set("view engine", "ejs");
 
 // express body parser middleware
 app.use(express.urlencoded({ extended: true }));
+
+// express session middleware
+app.use(
+    session({
+        secret: "secret",
+        resave: true,
+        saveUninitialized: true,
+    })
+);
+
+// connect flash
+app.use(flash());
+
+// // global vars
+// app.use((req, res, next) => {
+//     res.locals.success_msg = req.flash("success_msg");
+//     res.locals.error_msg = req.flash("error_msg");
+//     next();
+// });
 
 // establish routes
 app.use("/", require("./routes/index"));
