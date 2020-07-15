c818e54074e2b56fb51915b2886eaa7a8b30927f
hbull5 <harcourtbullv@gmail.com>
2020-07-15 09:31:58 -0400

    added starter comments in routes/index.js + sample db query in server.js

--- routes/index.js
+++ routes/index.js
@@ -6,37 +6,42 @@ router.use("/public", express.static("public"));
 
 // message response page
 router.get("/response", (req, res) => {
-	res.render("response");
+    res.render("response");
 });
 
 // patient record page
 router.get("/records", (req, res) => {
-	res.render("records");
+    res.render("records");
 });
 
 // quest designer page
 router.get("/quest", (req, res) => {
-	res.render("quest");
+    // query the db to get all patients assigned to logged in user (doctor) probably store them in a variable
+
+    // validation to make sure all the fields are properly filled out not empty
+
+    // you'll want to pass in anything you want to use in quest.ejs here as a second parameter of the render function
+    res.render("quest");
 });
 
 // new patient page
 router.get("/new", (req, res) => {
-	res.render("new");
+    res.render("new");
 });
 
 // messages page
 router.get("/messages", (req, res) => {
-	res.render("messages");
+    res.render("messages");
 });
 
 // account info page
 router.get("/info", (req, res) => {
-	res.render("info");
+    res.render("info");
 });
 
 // help FAQ page
 router.get("/help", (req, res) => {
-	res.render("help");
+    res.render("help");
 });
 
 module.exports = router;
