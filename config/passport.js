193b14be13d1ac214409d1726faf269ae3859500
hbull5 <harcourtbullv@gmail.com>
2020-07-19 10:11:35 -0400

    auth like 90% done needs hashed password comparison + page protection

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,70 @@
 const LocalStrategy = require("passport-local").Strategy;
-const bcrypt = require("bcryptjs");
 const db = require("./db");
+const bcrypt = require("bcryptjs");
 
-module.exports = (passport) => {};
+module.exports = function (passport) {
+	// =========================================================================
+	// LOCAL LOGIN =============================================================
+	// =========================================================================
+	// we are using named strategies since we have one for login and one for signup
+	// by default, if there was no name, it would just be called 'local'
+
+	passport.use(
+		"local",
+		new LocalStrategy(
+			{
+				// by default, local strategy uses username and password, we will override with email
+				usernameField: "username",
+				passwordField: "password",
+				passReqToCallback: true // allows us to pass back the entire request to the callback
+			},
+			function (req, username, password, done) {
+				// callback with email and password from our form
+
+				db.query(`SELECT * FROM users_medical WHERE username = ?`, [username], (err, results) => {
+					if (err) return done(err);
+					if (!results.length) {
+						return done(null, false, req.flash("error_msg", "No user found."));
+					}
+
+					if (results[0].password != password) {
+						return done(null, false, req.flash("error_msg", "Incorrect username & password!"));
+					}
+
+					return done(null, results[0]);
+				});
+
+				// connection.query("SELECT * FROM `users` WHERE `email` = '" + email + "'", function (err, rows) {
+				// 	if (err) return done(err);
+				// 	if (!rows.length) {
+				// 		return done(null, false, req.flash("loginMessage", "No user found.")); // req.flash is the way to set flashdata using connect-flash
+				// 	}
+
+				// 	// if the user is found but the password is wrong
+				// 	if (!(rows[0].password == password)) return done(null, false, req.flash("loginMessage", "Oops! Wrong password.")); // create the loginMessage and save it to session as flashdata
+
+				// 	// all is well, return successful user
+				// 	return done(null, rows[0]);
+				// });
+			}
+		)
+	);
+
+	// =========================================================================
+	// passport session setup ==================================================
+	// =========================================================================
+	// required for persistent login sessions
+	// passport needs ability to serialize and unserialize users out of session
+
+	// used to serialize the user for the session
+	passport.serializeUser((user, done) => {
+		done(null, user.doctorID);
+	});
+
+	// used to deserialize the user
+	passport.deserializeUser((id, done) => {
+		db.query("SELECT * FROM users_medical WHERE doctorID = ?", [id], (err, rows) => {
+			done(err, rows[0]);
+		});
+	});
+};
--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const flash = require("connect-flash");
 const session = require("express-session");
+const passport = require("passport");
+
+// passport config
+require("./config/passport")(passport);
 
 // set ejs as view engine
 app.set("view engine", "ejs");
@@ -12,21 +16,25 @@ app.use(express.urlencoded({ extended: true }));
 
 // express session middleware
 app.use(
-    session({
-        secret: "secret",
-        resave: true,
-        saveUninitialized: true,
-    })
+	session({
+		secret: "secret",
+		resave: true,
+		saveUninitialized: true
+	})
 );
+
+// passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 // connect flash
 app.use(flash());
 
 // global vars
 app.use((req, res, next) => {
-    res.locals.success_msg = req.flash("success_msg");
-    res.locals.error_msg = req.flash("error_msg");
-    next();
+	res.locals.success_msg = req.flash("success_msg");
+	res.locals.error_msg = req.flash("error_msg");
+	next();
 });
 
 // establish routes
